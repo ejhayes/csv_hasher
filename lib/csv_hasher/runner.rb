3dd066d3366d5f8cf531122630a1a165a046c875
Eric Hayes <ejhayes@jacobianengineering.com>
2014-05-16 18:28:46 -0700

    Updated the way lines are counted.

--- lib/csv_hasher/runner.rb
+++ lib/csv_hasher/runner.rb
@@ -4,7 +4,7 @@ module CsvHasher
       @source_filename = params[:source_filename]
       @target_filename = params[:target_filename] || "hashed_#{@source_filename}"
 
-      @lines = CSV.open(@source_filename).readlines.size
+      @lines = %x{wc -l #{@source_filename}}.split.first.to_i
 
       # open up our target csv and write the same headers as we have
       # for hte source target
